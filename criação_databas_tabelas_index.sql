f6a8409c01a9dca78710ebf9544703b0d8ccd653
Fábio da Silva Costa <biophas@gmail.com>
2018-10-03 16:07:37 -0300

    update
    update

--- criação_databas_tabelas_index.sql
+++ criação_databas_tabelas_index.sql
@@ -2,7 +2,7 @@ use curso_sql;
 
 create table funcionarios 
 (
-	id int unsigned not null auto_increment,
+    id int unsigned not null auto_increment,
     nome varchar(45) not null,
     salario double not null default '0',
     departamento varchar(45) not null,
@@ -12,18 +12,18 @@ create table funcionarios
 create table veiculos 
 
 (
-	id int unsigned not null auto_increment,
+    id int unsigned not null auto_increment,
     funcionario_id int unsigned default null,
     veiculo varchar(45) not null default '',
     placa varchar(10) not null default '',
-	primary key (id),
+    primary key (id),
     constraint fk_veiculos_funcionarios foreign key (funcionario_id) references funcionarios (id)
 );
 
 create table salarios 
 
 (
-	faixa varchar(45) not null,
+    faixa varchar(45) not null,
     inicio double not null,
     fim double not null,
     primary key (faixa)
